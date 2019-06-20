0fdcabc25799e63ac65b1da01218342211fcc2b9
alk831 <adam54111@gmail.com>
2019-06-20 21:41:59 +0200

    scaffold webpack conf

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.tsx',
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  },
+  output: {
+    path: path.join(__dirname, '/build'),
+    filename: 'build.min.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'awesome-typescript-loader'
+      },
+      {
+        test: /\.css$/i,
+        loader: 'css-loader'
+      }
+    ]
+  }
+}
